1ff3c08659c7c9f044eb069674091ac86d029ddd
Sel-Kata <60038461+Sel-Kata@users.noreply.github.com>
2022-04-11 15:33:42 +0300

    Create algorithms.cpp

---
+++ algorithms.cpp
@@ -0,0 +1,65 @@
+#include <iostream>
+using namespace std;
+
+template<typename T>
+bool all_of(T begin, T end, bool(*func)(T)){//все элементы удовлетворяют
+    while(begin!=end){
+        if(func(*begin)==false){
+            return false;
+        }
+        ++begin;
+    }
+    return true;
+}
+template<typename T>
+bool any_of(T begin, T end, bool(*func)(T)){//хотяб 1 эл удовлетворяет
+    while(begin!=end){
+        if(func(*begin)==true){
+            return true;
+        }
+        ++begin;
+    }
+    return false;
+}
+template<typename T>
+bool none_of(T begin, T end, bool(*func)(T)){//
+    while(begin!=end){
+        if(func(*begin)==true){
+            return false;
+        }
+        ++begin;
+    }
+    return true;
+}
+template<typename T>
+bool one_of(T begin, T end, bool(*func)(T)){//ровно 1 удовлетворяет
+    int count=0;
+    while(begin!=end){
+        if(func(*begin)==true){
+            count++;
+        }
+        ++begin;
+    }
+    if (count==1){
+        return true;
+    }
+    else{
+        return false;
+    }
+
+}
+template<typename T>
+bool func(T value){
+    if (sizeof(value)>sizeof(int)){
+        return true;
+    }
+    else{
+        return false;
+    }
+}
+
+int main(){
+    int array[]={8,7,6,5};
+    one_of(array, array+4, func);
+
+}
